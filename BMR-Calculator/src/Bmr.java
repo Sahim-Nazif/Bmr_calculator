5207b2c886cabe6ed560266c93d995d42cfca808
Sahim-Nazif <nsahim@my.centennialcollege.ca>
2018-09-20 19:40:28 -0400

    Created Scanner for input

--- BMR-Calculator/src/Bmr.java
+++ BMR-Calculator/src/Bmr.java
@@ -1,7 +1,18 @@
-
+import java.util.Scanner;
 public class Bmr {
 
 	public static void main(String[] args) {
+	Scanner input=new Scanner(System.in);
+	
+	while (true) {
+		System.out.println("Welcome to BMR calculator ");
+		System.out.println("--------------------------");
+		System.out.println("Please Enter Your Gender");
+		String gend=input.nextLine();
+		if (gend.equalsIgnoreCase("male")) {
+			
+		}
+	}
 	
 
 	}
