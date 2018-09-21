27c271dc14e16f1cdb410007e31b6b81a2799bee
Sahim-Nazif <nsahim@my.centennialcollege.ca>
2018-09-20 20:11:15 -0400

    Added else if statement to exit application

--- BMR-Calculator/src/Bmr.java
+++ BMR-Calculator/src/Bmr.java
@@ -7,6 +7,7 @@ public class Bmr {
 	System.out.println("--------------------------------");
 	while (true) {
 		System.out.println("Please Enter Your Gender");
+		System.out.println("To quit the application type exit");
 		String gend=input.nextLine();
 		if (gend.equalsIgnoreCase("male")) {
 			System.out.println("Enter your height in cm :");
@@ -19,18 +20,19 @@ public class Bmr {
 			System.out.printf("Your BMR is %f calories per day\n", bmr);
 			
 		}else if (gend.equalsIgnoreCase("female")) {
-			  System.out.println("Enter your height in cm :");
-				double height=input.nextDouble();
-				System.out.println("Enter your weight in kg : ");
-				double weight=input.nextDouble();
-				System.out.println("Enter your age :");
-				int age=input.nextInt();
-				double bmr= 655.1 + ( 9.56* weight) + ( 1.850 * height)-( 4.676 * age );
-				System.out.printf("Your BMR is %f calories per day\n", bmr);
+		    System.out.println("Enter your height in cm :");
+			 double height=input.nextDouble();
+			 System.out.println("Enter your weight in kg : ");
+			 double weight=input.nextDouble();
+			 System.out.println("Enter your age :");
+			 int age=input.nextInt();
+			 double bmr= 655.1 + ( 9.56* weight) + ( 1.850 * height)-( 4.676 * age );
+			 System.out.printf("Your BMR is %f calories per day\n", bmr);
+		}else if (gend.equalsIgnoreCase("exit")) {
+			System.out.println("Have a nice day !");
+			break;
 		}
+		
 	}
-	
-
-	}
-
+	}	
 }
